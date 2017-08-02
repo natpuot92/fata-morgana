9c3982cb6d562d254d089f886f8ceeed507b7ff7
natpuot92 <natpuot1992@gmail.com>
2017-08-02 20:55:00 +0300

    commit

---
+++ app/initialize.js
@@ -0,0 +1,36 @@
+import $ from "jquery"
+import slick from 'slick-carousel'
+
+function initSlick () {
+  $('.features-list').slick({
+    infinite: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+    ]
+  })
+}
+
+$(document).ready(() => {
+  initSlick()
+})
+
+
