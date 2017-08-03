0e9ed3eefa36177edfa763e8617199b8cfb63a6c
natpuot92 <natpuot1992@gmail.com>
2017-08-03 16:37:01 +0300

    commit

--- app/initialize.js
+++ app/initialize.js
@@ -1,36 +1,12 @@
 import $ from "jquery"
 import slick from 'slick-carousel'
 
-function initSlick () {
-  $('.features-list').slick({
-    infinite: true,
-    arrows: false,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-    ]
-  })
-}
+ $('.us-social__slider').slick({
+   slidesToShow: 1,
+   centerMode: true
+ });
 
-$(document).ready(() => {
-  initSlick()
-})
-
-
+$('.reviews__slider').slick({
+    centerMode: true,
+    slidesToShow: 1,
+});
