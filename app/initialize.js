d4878ba10d300e5fb4ff33147652f0cfacb6b1df
natpuot92 <natpuot1992@gmail.com>
2017-08-04 02:01:16 +0300

    commit

--- app/initialize.js
+++ app/initialize.js
@@ -2,11 +2,29 @@ import $ from "jquery"
 import slick from 'slick-carousel'
 
  $('.us-social__slider').slick({
-   slidesToShow: 1,
-   centerMode: true
+    slidesToShow: 1,
+    fade: true,
+    centerMode: true,
+    appendArrows: $('.us-social__slider-arrows'),
+    prevArrow: '<button id="prev" type="button" class="slider-prev us-social-prev"></button>',
+    nextArrow: '<button id="prev" type="button" class="slider-next us-social-next"></button>'
+
  });
 
 $('.reviews__slider').slick({
     centerMode: true,
     slidesToShow: 1,
+    appendArrows: $('.reviews__slider-arrow'),
+    prevArrow: '<button id="prev" type="button" class="slider-prev reviews-prev"></button>',
+    nextArrow: '<button id="prev" type="button" class="slider-next reviews-next"></button>'
 });
+
+$('.popup__slids').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    pauseOnHover: true,
+    appendArrows: $('.popup-slider__arrows'),
+    prevArrow: '<button id="prev" type="button" class="btn section3-btn-prev popup-slider-prev"><span class = "section3-btn-prev-text popup__slider-prev-text">Назад</span><div class="section3-btn-prev-arrow"></div></button>',
+    nextArrow: '<button id="next" type="button" class="btn section3-btn-next popup-slider-next"><span class = "section3-btn-next-text popup__slider-next-text">Вперед</span><div class="section3-btn-next-arrow"></div></button>'
+  });
