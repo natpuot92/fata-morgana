6c816b3eccc584a01144697b2a37fe480534104d
natpuot92 <natpuot1992@gmail.com>
2017-08-04 10:46:11 +0300

    commit

--- app/initialize.js
+++ app/initialize.js
@@ -19,12 +19,4 @@ $('.reviews__slider').slick({
     nextArrow: '<button id="prev" type="button" class="slider-next reviews-next"></button>'
 });
 
-$('.popup__slids').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true,
-    pauseOnHover: true,
-    appendArrows: $('.popup-slider__arrows'),
-    prevArrow: '<button id="prev" type="button" class="btn section3-btn-prev popup-slider-prev"><span class = "section3-btn-prev-text popup__slider-prev-text">Назад</span><div class="section3-btn-prev-arrow"></div></button>',
-    nextArrow: '<button id="next" type="button" class="btn section3-btn-next popup-slider-next"><span class = "section3-btn-next-text popup__slider-next-text">Вперед</span><div class="section3-btn-next-arrow"></div></button>'
-  });
+
